59ca26190bbbbfac579a25f42899a9d24fd663b1
Raman <24293550+ramank775@users.noreply.github.com>
2025-03-16 18:25:55 +0530

    Revert "fix permission issue for /run/dovecot"
    This reverts commit 9c858760aa3ebc7d7935a2a9a5595e07848b96f7.

--- deployment/mail/variables.tf
+++ deployment/mail/variables.tf
@@ -6,7 +6,7 @@ variable "namespace" {
 
 variable "dovecot-tag" {
   description = "dovecot image tag"
-  default     = "2.3.21"
+  default     = "2.3.20"
   sensitive   = false
   type        = string
 }
